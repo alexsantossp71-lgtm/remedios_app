00ac9c7217c32269b8ffb4dbebcbeee23c03aa8c
alexsantossp71-lgtm <alexsantossp71@gmail.com>
2025-09-04 15:53:26 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-// ATENÇÃO: Substitua '<NOME_DO_SEU_REPOSITORIO>' pelo nome exato do seu repositório no GitHub.
-// Exemplo: se a URL for github.com/seu-usuario/meu-app-remedios, o nome é 'meu-app-remedios'.
-export default defineConfig({
-  base: '/remedios_app/',
-});
